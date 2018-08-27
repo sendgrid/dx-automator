410c83bb8fdf1cb817bef8f79e2d7b85a20da27c
James Purpura <purpurajames@gmail.com>
2018-08-27 16:43:11 -0700

    fix git issues

--- services/looker/manage.py
+++ services/looker/manage.py
@@ -1,21 +1,9 @@
 import unittest
 
-import coverage
 from flask.cli import FlaskGroup
 
 from project import create_app, db
 from project.api.models import Looker
-
-
-COV = coverage.coverage(
-    branch=True,
-    include="project/*",
-    omit=[
-        "project/tests/*",
-        "project/config.py",
-    ]
-)
-COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -44,22 +32,6 @@ def seed_db():
     db.session.add(Looker(username="james", email="james@sendgrid.com"))
     db.session.add(Looker(username="purpura", email="purpura@sendgrid.com"))
     db.session.commit()
-
-
-@cli.command()
-def cov():
-    """Run the unit tests with coverage."""
-    tests = unittest.TestLoader().discover("project/tests")
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        COV.stop()
-        COV.save()
-        print("Coverage Summary:")
-        COV.report()
-        COV.html_report()
-        COV.erase()
-        return 0
-    return 1
 
 
 if __name__ == '__main__':
--- services/looker/project/tests/test_looker.py
+++ services/looker/project/tests/test_looker.py
@@ -142,42 +142,6 @@ class TestLookerService(BaseTestCase):
             self.assertIn("purpura@sendgrid.com", data["data"]["users"][1]["email"])
             self.assertIn("success", data["status"])
 
-    def test_main_no_users(self):
-        """
-        Ensure the main route behaves correctly when no users
-        have been added to the database.
-        """
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"All Users", response.data)
-        self.assertIn(b"<p>No users!</p>", response.data)
-
-    def test_main_with_users(self):
-        """Ensure the main route behaves correctly when users have been
-        added to the database."""
-        add_user("james", "james@sendgrid.com")
-        add_user("purpura", "purpura@sendgrid.com")
-        with self.client:
-            response = self.client.get('/')
-            self.assertEqual(response.status_code, 200)
-            self.assertIn(b'All Users', response.data)
-            self.assertNotIn(b'<p>No users!</p>', response.data)
-            self.assertIn(b"james", response.data)
-            self.assertIn(b"purpura", response.data)
-
-    def test_main_add_user(self):
-        """Ensure a new user can be add to the database"""
-        with self.client:
-            response = self.client.post(
-                "/",
-                data=dict(username="james", email="james@sendgrid.com"),
-                follow_redirects=True
-            )
-            self.assertEqual(response.status_code, 200)
-            self.assertIn(b"All Users", response.data)
-            self.assertNotIn(b"<p>No users!</p>", response.data)
-            self.assertIn(b"james", response.data)
-
 
 if __name__ == "__main__":
     unittest.main()
