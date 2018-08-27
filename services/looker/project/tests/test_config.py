e788bf6a7c0f6d62ff8477f11ec361fe86dc8738
James Purpura <purpurajames@gmail.com>
2018-08-27 16:28:47 -0700

    add debug toolbar to tests

--- services/looker/project/tests/test_config.py
+++ services/looker/project/tests/test_config.py
@@ -21,6 +21,7 @@ class TestDevelopmentConfig(TestCase):
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_URL')
         )
+        self.assertTrue(app.config["DEBUG_TB_ENABLED"])
 
 
 class TestTestingConfig(TestCase):
@@ -36,6 +37,7 @@ class TestTestingConfig(TestCase):
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_TEST_URL')
         )
+        self.assertFalse(app.config["DEBUG_TB_ENABLED"])
 
 
 class TestProductionConfig(TestCase):
@@ -46,6 +48,7 @@ class TestProductionConfig(TestCase):
     def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config["DEBUG_TB_ENABLED"])
 
 
 if __name__ == '__main__':
