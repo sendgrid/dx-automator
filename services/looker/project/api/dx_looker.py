44b71df66932f99ccf03e46e794c39510342f523
James Purpura <purpurajames@gmail.com>
2018-08-27 16:45:36 -0700

    rename to dx looker

--- services/looker/project/api/dx_looker.py
+++ services/looker/project/api/dx_looker.py
@@ -3,16 +3,20 @@ import json
 import os
 import pprint
 
+
 def parse_dx_look_json(json_dict: dict):
     pprint.pprint(json_dict)
 
-class DxLookerApi(object):
+
+class DxLooker(object):
     def __init__(self, client_id: str, client_secret: str, endpoint: str):
         self._client_id = client_id
         self._client_secret = client_secret
         self._token = None
+
         self.endpt = endpoint
         self.session = requests.session()
+        
     def authenticate(self):
         """Updates session with Looker token from given client credentials"""
         params = {"client_id": self._client_id,
@@ -35,7 +39,7 @@ if __name__ == "__main__":
     sg_client_id = os.environ.get("LOOKER_CLIENT_ID")
     sg_client_secret = os.environ.get("LOOKER_CLIENT_SECRET")
     sg_endpoint = os.environ.get("SENDGRID_LOOKER")
-    looker_api = DxLookerApi(sg_client_id, sg_client_secret, sg_endpoint)
+    looker_api = DxLooker(sg_client_id, sg_client_secret, sg_endpoint)
     looker_api.authenticate()
     json_object = looker_api.run_look("4405").json()
     parse_dx_look_json(json_object)
