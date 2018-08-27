e9557c9456e969b400166293932bed8876de76f2
James Purpura <purpurajames@gmail.com>
2018-08-27 16:30:08 -0700

    add Rest API object for looker

--- services/looker/project/api/dx_looker_api.py
+++ services/looker/project/api/dx_looker_api.py
@@ -1,9 +1,14 @@
 import requests
 import json
 import os
+import pprint
 
 
-class LookerApi(object):
+def parse_dx_look_json(json_dict: dict):
+    pprint.pprint(json_dict)
+
+
+class DxLookerApi(object):
     def __init__(self, client_id: str, client_secret: str, endpoint: str):
         self._client_id = client_id
         self._client_secret = client_secret
@@ -13,6 +18,7 @@ class LookerApi(object):
         self.session = requests.session()
 
     def authenticate(self):
+        """Updates session with Looker token from given client credentials"""
         params = {"client_id": self._client_id,
                   "client_secret": self._client_secret}
         response = self.session.post("{}/login".format(self.endpt),
@@ -23,17 +29,18 @@ class LookerApi(object):
         )
 
     def run_look(self, look_id: str, result_format="json"):
+        """Returns response from GET of specified look_id"""
         return looker_api.session.get("{}/api/3.0/looks/{}/run/{}".format(
             self.endpt, look_id, result_format)
         )
 
 
 if __name__ == "__main__":
-    # os.environ["LOOKER_CLIENT_ID"] = "f2t6FYVTJYhqwNP2S76w"
     sg_client_id = os.environ.get("LOOKER_CLIENT_ID")
     sg_client_secret = os.environ.get("LOOKER_CLIENT_SECRET")
     sg_endpoint = os.environ.get("SENDGRID_LOOKER")
-    looker_api = LookerApi(sg_client_id, sg_client_secret, sg_endpoint)
+    looker_api = DxLookerApi(sg_client_id, sg_client_secret, sg_endpoint)
     looker_api.authenticate()
-    print(looker_api.run_look("4405").json())
+    json_object = looker_api.run_look("4405").json()
+    parse_dx_look_json(json_object)
 
