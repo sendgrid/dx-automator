07177bc1092283142b2d24acb5ec79e78557634c
af4ro <1997anshul@gmail.com>
2018-08-27 17:19:12 -0700

    Added more UI componenets

--- services/client-react/src/App.js
+++ services/client-react/src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
-import { PageHeader, PageHeading, Button, Divider } from "@sendgrid/ui-components";
+import { PageHeader, PageHeading, Divider,
+  DropdownButton, Card, Tab, Tabs} from "@sendgrid/ui-components";
 import './App.css';
 
 class App extends Component {
+  getTasks(){
+    // return axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+  };
+
   render() {
     return (
-      <div className="App">
-        <PageHeader>
-          <PageHeading title="DX-automator">
-            <Button type="primary" id="stui-test-locator-abc">
-              Primary
-            </Button>
-          </PageHeading>
-        </PageHeader>
-        <Divider/>
+      <div className="App" id="app-maindiv">
+        <div className="Header">
+          <PageHeader>
+            <PageHeading title="DX-automator">
+              <DropdownButton label="Settings" type="secondary" icon="gear">
+                <a>Work</a>
+                <a>in</a>
+                <a>Progress</a>
+              </DropdownButton>
+            </PageHeading>
+          </PageHeader>
+          <Divider id="divider"/>
+        </div>
+
+        <div className="Body">
+          <Tabs id="Tabs" zeroBorder centered>
+            <Tab active>Tasks</Tab>
+            <Tab>Coming</Tab>
+            <Tab>Soon</Tab>
+          </Tabs>
+          <div className="list">
+            <div className="col-8" id="tasks-card">
+              <Card
+              // body={this.getTasks}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
