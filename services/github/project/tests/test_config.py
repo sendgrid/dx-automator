3b1c5fefadeeacc3a4c390b48273c84b8681ad74
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2018-10-26 00:30:14 +0530

    Migrate to Github API v4

--- services/github/project/tests/test_config.py
+++ services/github/project/tests/test_config.py
@@ -5,14 +5,16 @@ from flask_testing import TestCase
 from project import create_app
 app = create_app()
 
+
 class TestDevelopmentConfig(TestCase):
     def create_app(self):
         app.config.from_object('project.config.DevelopmentConfig')
         return app
 
     def test_app_is_development(self):
-        self.assertTrue(app.config['DEBUG'] is True)
-        self.assertFalse(current_app is None)
+        self.assertTrue(app.config['DEBUG'])
+        self.assertIsNotNone(current_app)
+
 
 class TestTestingConfig(TestCase):
     def create_app(self):
@@ -22,6 +24,8 @@ class TestTestingConfig(TestCase):
     def test_app_is_testing(self):
         self.assertFalse(app.config['DEBUG'])
         self.assertTrue(app.config['TESTING'])
+        self.assertTrue(app.config['EXCEPTIONS'])
+
 
 class TestProductionConfig(TestCase):
     def create_app(self):
@@ -31,6 +35,7 @@ class TestProductionConfig(TestCase):
     def test_app_is_production(self):
         self.assertFalse(app.config['DEBUG'])
         self.assertFalse(app.config['TESTING'])
+
 
 if __name__ == '__main__':
     unittest.main()
--- services/github/project/tests/test_github.py
+++ services/github/project/tests/test_github.py
@@ -2,39 +2,73 @@ import json
 
 from project.tests.base import BaseTestCase
 
+
 class TestGitHubService(BaseTestCase):
     """Tests for the GitHub Service."""
 
     def test_is_member(self):
         """Ensure the user is a member of your GitHub organization."""
-        #TODO: This should be configurable
+        # TODO: This should be configurable
         test_users = [
-            'mbernier',
-            'thinkingserious',
+            'mitsuhiko',
+            'ThiefMaster',
+            'lepture',
+            'jeffwidman',
+            'untitaker',
+            'edk0',
+            'davidism',
+            'pgjones',
+            'miguelgrinberg',
+            'dawran6',
+            'keyan',
+            'greyli',
+            'jcrotts',
             'test_exception_user'
         ]
         for user in test_users:
-            response = self.client.get(f'/is_member/{user}')
+            response = self.client.get(f'github/is_member/{user}')
+            if response.status_code != 200:
+                pass # raise AttributeError(user)
             self.assertEqual(response.status_code, 200)
             data = json.loads(response.data.decode())
             self.assertTrue(data['is_member'])
 
-        response = self.client.get(f'/is_member/fake_user')
+        response = self.client.get(f'github/is_member/octocat')
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 404)
         self.assertFalse(data['is_member'])
 
+        token, self.app.config['GITHUB_TOKEN'] = self.app.config['GITHUB_TOKEN'], 'token'
+        response = self.client.get(f'github/is_member/octocat')
+        self.assertEqual(response.status_code, 400)
+        self.app.config['GITHUB_TOKEN'] = token
+
     def test_get_all_members(self):
         """Ensure all the users are a member of your GitHub organization."""
-        #TODO: This should be configurable
+        # TODO: This should be configurable
         test_users = [
-            'mbernier',
-            'thinkingserious',
-            'test_exception_user'
+            'mitsuhiko',
+            'ThiefMaster',
+            'lepture',
+            'jeffwidman',
+            'untitaker',
+            'edk0',
+            'davidism',
+            'pgjones',
+            'miguelgrinberg',
+            'dawran6',
+            'keyan',
+            'greyli',
+            'jcrotts'
         ]
-        response = self.client.get(f'/members')
+        response = self.client.get(f'github/members')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode())
-        self.assertEqual(test_users, json.loads(response.data.decode()))
-       
+        if response.status_code != 200:
+            pass # raise AttributeError(len(data))
+        self.assertEqual(test_users, data)
 
+        token, self.app.config['GITHUB_TOKEN'] = self.app.config['GITHUB_TOKEN'], 'token'
+        response = self.client.get(f'github/members')
+        self.assertEqual(response.status_code, 400)
+        self.app.config['GITHUB_TOKEN'] = token
