0aaaed516e29e1aa91aac60697c9451d779e05f7
Prabakaran Kumaresshan <k_prabakaran@hotmail.com>
2018-10-29 21:33:04 +0530

    Cleanup Testcases

--- services/github/project/tests/test_github.py
+++ services/github/project/tests/test_github.py
@@ -6,6 +6,10 @@ from project.tests.base import BaseTestCase
 class TestGitHubService(BaseTestCase):
     """Tests for the GitHub Service."""
 
+    def test_health_check(self):
+        response = self.client.get('github/ping')
+        self.assertEqual(response.status_code, 200)
+
     def test_is_member(self):
         """Ensure the user is a member of your GitHub organization."""
         # TODO: This should be configurable
@@ -27,8 +31,6 @@ class TestGitHubService(BaseTestCase):
         ]
         for user in test_users:
             response = self.client.get(f'github/is_member/{user}')
-            if response.status_code != 200:
-                pass # raise AttributeError(user)
             self.assertEqual(response.status_code, 200)
             data = json.loads(response.data.decode())
             self.assertTrue(data['is_member'])
@@ -64,8 +66,6 @@ class TestGitHubService(BaseTestCase):
         response = self.client.get(f'github/members')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode())
-        if response.status_code != 200:
-            pass # raise AttributeError(len(data))
         self.assertEqual(test_users, data)
 
         token, self.app.config['GITHUB_TOKEN'] = self.app.config['GITHUB_TOKEN'], 'token'
