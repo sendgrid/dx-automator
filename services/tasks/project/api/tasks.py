dd594e224d507bc7d4348b65220733252a5b5344
Elmer Thomas <elmer@thinkingserious.com>
2019-05-13 23:09:18 -0700

    Fix task tests

--- services/tasks/project/api/tasks.py
+++ services/tasks/project/api/tasks.py
@@ -16,7 +16,7 @@ tasks_blueprint = Blueprint('tasks', __name__, template_folder='./templates')
 def index():
     if request.method == 'POST':
         db.session.add(Task(creator=request.form['creator'],
-                       link=request.form['link']))
+                       url=request.form['url']))
         db.session.commit()
     tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
@@ -42,19 +42,19 @@ def add_single_task():
 
     # TODO: Add the other optional paramaters here
     creator = post_data.get('creator')
-    link = post_data.get('link')
+    url = post_data.get('url')
     try:
-        task = Task.query.filter_by(link=link).first()
+        task = Task.query.filter_by(url=url).first()
         if not task:
-            db.session.add(Task(creator=creator, link=link))
+            db.session.add(Task(creator=creator, url=url))
             db.session.commit()
             response_object = {
                 'status': 'success',
-                'message': f'{link} was added!'
+                'message': f'{url} was added!'
             }
             return jsonify(response_object), 201
         else:
-            response_object['message'] = 'Sorry. That link already exists.'
+            response_object['message'] = 'Sorry. That url already exists.'
             return jsonify(response_object), 400
     except exc.IntegrityError:
         db.session.rollback()
@@ -76,7 +76,7 @@ def get_single_task(task_id):
                 'status': 'success',
                 'data': {
                     'id': task.id,
-                    'link': task.link,
+                    'url': task.url,
                     'creator': task.creator
                 }
             }
