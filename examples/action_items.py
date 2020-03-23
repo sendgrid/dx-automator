360e162f39c018fd232380893edae5af6ba4ad9e
Sam Harrison <sharrison@twilio.com>
2020-03-23 14:03:32 -0500

    feat: add aging issues to the action items report

--- examples/action_items.py
+++ examples/action_items.py
@@ -6,8 +6,9 @@ from examples.common.admins import ADMINS
 from examples.common.issue import get_author, get_issues, Issue, substitute
 from examples.common.repos import ALL_REPOS
 
-STUCK_DELTA = timedelta(days=30)
-STUCK_DATE_STR = str(date.today() - STUCK_DELTA)
+STUCK_DATE = str(date.today() - timedelta(days=30))
+BUG_DATE = str(date.today() - timedelta(days=20))
+ENHANCEMENT_DATE = str(date.today() - timedelta(days=60))
 
 
 class ActionItemsCollector:
@@ -16,6 +17,8 @@ class ActionItemsCollector:
         self.contact_needed = []
         self.response_needed = {}
         self.stuck_waiting = {}
+        self.open_bugs = []
+        self.open_enhancements = []
 
         for admin in ADMINS:
             self.response_needed[admin] = []
@@ -32,10 +35,14 @@ class ActionItemsCollector:
             self.print_issues(f'{admin} - Issues needing response', self.response_needed[admin])
             self.print_issues(f'{admin} - Issues stuck waiting', self.stuck_waiting[admin])
 
+        self.print_issues('Aging bugs', self.open_bugs)
+        self.print_issues('Aging enhancements', self.open_enhancements)
+
     def print_issues(self, title: str, issues: List[Issue]):
         if issues:
             print(f'\n{title}:')
-            print('\n'.join([issue.url for issue in issues]))
+            print('\n'.join([issue.url for issue in sorted(issues,
+                                                           key=lambda issue: issue.created_at)]))
 
     def process_repo(self, org: str, repo: str, start_date: str) -> None:
         issues = get_open_items(org, repo, start_date)
@@ -43,10 +50,15 @@ class ActionItemsCollector:
         for issue_json in issues:
             issue = Issue(issue_json)
 
-            if issue.author in ADMINS:
+            if issue.author in ADMINS or issue.merged:
                 continue
 
             issue.process_events()
+
+            # Required because some merged PRs come back as open.
+            # E.g., https://github.com/sendgrid/python-http-client/pull/132
+            if issue.merged:
+                continue
 
             if 'time_to_contact' not in issue.metrics and 'time_to_contact_pr' not in issue.metrics:
                 if issue.is_pr:
@@ -54,13 +66,19 @@ class ActionItemsCollector:
                         self.contact_needed.append(issue)
                 else:
                     self.contact_needed.append(issue)
-
-            if issue.is_waiting_for_response:
+            elif issue.is_waiting_for_response:
                 self.response_needed[get_author(issue.last_admin_comment)].append(issue)
-            elif issue.waiting_for_feedback and \
-                issue.waiting_for_feedback['createdAt'] < STUCK_DATE_STR and \
-                issue.last_admin_comment['createdAt'] < STUCK_DATE_STR:
-                self.stuck_waiting[get_author(issue.last_admin_comment)].append(issue)
+            elif issue.waiting_for_feedback:
+                if issue.waiting_for_feedback['createdAt'] < STUCK_DATE and \
+                    issue.last_admin_comment['createdAt'] < STUCK_DATE:
+                    self.stuck_waiting[get_author(issue.last_admin_comment)].append(issue)
+            else:
+                if issue.get_issue_type() == 'bug':
+                    if issue.created_at < BUG_DATE:
+                        self.open_bugs.append(issue)
+                else:
+                    if issue.created_at < ENHANCEMENT_DATE:
+                        self.open_enhancements.append(issue)
 
 
 @lru_cache(maxsize=None)
@@ -107,10 +125,12 @@ def get_open_items(org: str, repo: str, start_date: str):
         },
         {
             'issue_type': 'PullRequest',
-            'additional_event_names': ['PULL_REQUEST_COMMIT', 'PULL_REQUEST_REVIEW'],
+            'additional_event_names': ['PULL_REQUEST_COMMIT', 'PULL_REQUEST_REVIEW',
+                                       'MERGED_EVENT'],
             'additional_event_types': [
                 '... on PullRequestCommit {commit {committedDate author {user {login}} status {state}}}',
-                '... on PullRequestReview {createdAt state author {login}}']
+                '... on PullRequestReview {createdAt state author {login}}',
+                '... on MergedEvent {createdAt}']
         }
     ]
 
@@ -122,4 +142,4 @@ def get_open_items(org: str, repo: str, start_date: str):
 
 
 if __name__ == '__main__':
-    ActionItemsCollector().run(start_date='2019-10-01')
+    ActionItemsCollector().run(start_date='2020-01-01')
