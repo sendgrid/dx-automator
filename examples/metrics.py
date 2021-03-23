fd6be8ea2ff66dc50fa8a28d5336816ca378af23
Elmer Thomas <ethomas@twilio.com>
2021-03-23 12:44:27 -0700

    chore: update 2021 SLO metrics (#115)

--- examples/metrics.py
+++ examples/metrics.py
@@ -1,4 +1,5 @@
 import statistics
+import sys
 from collections import defaultdict
 from datetime import datetime, timedelta
 from functools import lru_cache
@@ -7,12 +8,13 @@ from typing import Dict, List
 
 from common.admins import ADMINS
 from common.google_api import get_spreadsheets
-from common.issue import substitute, get_issues, Issue, get_delta_days, print_json, \
-    get_date_time
+from common.issue import substitute, get_issues, Issue, get_delta_days, get_date_time
 from common.repos import ALL_REPOS
 
 GOOGLE_SHEET_ID = '1cQOOT5aYxfXOSwEV0cJyf01KkV-uKCBJnKK3PHjouCE'
-GOOGLE_SHEET_NAME = 'Sheet1'
+GOOGLE_SHEET_NAME_DAILY = 'Daily'
+GOOGLE_SHEET_NAME_WEEKLY = 'Weekly'
+STALE_DAYS = 365
 
 
 def base_type():
@@ -41,12 +43,11 @@ class MetricCollector:
 
                 self.process_repo(repo_node, org, repo, start_date, end_date)
 
-        # If we have any untagged issues, print them and exit.
+        # If we have any untagged issues, print them
         if self.untagged_issues:
             print('These issues need a "type" label:')
             for issue in self.untagged_issues:
                 print(issue.url)
-            return
 
         for org in global_node['nodes']:
             org_node = global_node['nodes'][org]
@@ -55,25 +56,22 @@ class MetricCollector:
                 repo_node = org_node['nodes'][repo]
 
                 self.aggregate(repo_node)
-                self.summarize(repo, reporting_period, repo_node)
+                name = f'https://github.com/{org}/{repo}'
+                self.summarize(name, reporting_period, repo_node)
 
-            self.aggregate(org_node)
-            self.summarize(org, reporting_period, org_node)
-
-        self.aggregate(global_node)
-        self.summarize('global', reporting_period, global_node)
-
-        print_json(self.metrics)
-
-        self.output_google_sheet()
+        self.output_google_sheet(reporting_period=reporting_period)
 
     def process_repo(self, nodes: Dict,
                      org: str, repo: str,
                      start_date: str, end_date: str) -> None:
         start_date = get_date_time(start_date)
         end_date = get_date_time(end_date)
+        stale_date = datetime.strptime(datetime.now().strftime(DATE_TIME_FORMAT), DATE_TIME_FORMAT) - timedelta(days=STALE_DAYS)
+        stale_date = get_date_time(stale_date.strftime(DATE_TIME_FORMAT))
 
         issues = get_repo_issues(org, repo)
+        issue_count = 0
+        pr_count = 0
 
         for issue_json in issues:
             issue = Issue(issue_json, end_date=end_date)
@@ -82,6 +80,9 @@ class MetricCollector:
                 continue
 
             if issue.created_at > end_date:
+                continue
+
+            if issue.created_at < stale_date:
                 continue
 
             issue.process_events()
@@ -112,12 +113,26 @@ class MetricCollector:
                 if not issue.first_admin_comment:
                     issue.metrics.pop('time_to_close_pr', None)
 
-                nodes['nodes'][issue.url]['metrics'] = issue.metrics
+            nodes['nodes'][issue.url]['metrics'] = issue.metrics
 
-            if not issue.closed:
+            if issue.events != []:
+                if issue.events is not None:
+                    last_update = issue.events[-1].get('createdAt', None)
+                if last_update is None:
+                    last_update = issue.events[-1]['commit']['committedDate']
+            else:
+                last_update = issue.created_at
+
+            if not issue.closed and last_update > stale_date:
                 time_open = get_delta_days(issue.created_at, end_date)
-
-                nodes['nodes'][issue.url]['metrics']['time_open'] = time_open
+                if '/pull/' in issue.url:
+                    pr_count += 1
+                    nodes['nodes'][issue.url]['metrics']['pr_count'] = pr_count
+                    nodes['nodes'][issue.url]['metrics']['time_open_pr'] = time_open
+                else:
+                    issue_count += 1
+                    nodes['nodes'][issue.url]['metrics']['issue_count'] = issue_count
+                    nodes['nodes'][issue.url]['metrics']['time_open_issue'] = time_open
 
     def add_time_to_resolve(self, issue: Issue) -> None:
         for ext in {'', '_pr'}:
@@ -155,7 +170,7 @@ class MetricCollector:
             }
 
     def summarize(self, name: str, reporting_date: str, node: Dict) -> None:
-        repo_metrics = {'name': name, 'date': reporting_date}
+        repo_metrics = {'name': name, 'date': datetime.now().strftime(DATE_TIME_FORMAT)}
 
         metrics = node['metrics']
 
@@ -166,9 +181,14 @@ class MetricCollector:
 
         self.all_metrics.append(repo_metrics)
 
-    def output_google_sheet(self):
+    def output_google_sheet(self, reporting_period: str) -> None:
+        if reporting_period == 'daily':
+            google_sheet_name = GOOGLE_SHEET_NAME_DAILY
+        if reporting_period == 'weekly':
+            google_sheet_name = GOOGLE_SHEET_NAME_WEEKLY
+
         response = self.spreadsheets.values().get(spreadsheetId=GOOGLE_SHEET_ID,
-                                                  range=f'{GOOGLE_SHEET_NAME}!1:1').execute()
+                                                  range=f'{google_sheet_name}!1:1').execute()
         header = response.get('values', [[]])[0]
 
         values = []
@@ -186,12 +206,12 @@ class MetricCollector:
                     row.append(value)
 
         self.spreadsheets.values().update(spreadsheetId=GOOGLE_SHEET_ID,
-                                          range=f'{GOOGLE_SHEET_NAME}!1:1',
+                                          range=f'{google_sheet_name}!1:1',
                                           valueInputOption='USER_ENTERED',
                                           body={'values': [header]}).execute()
 
         self.spreadsheets.values().append(spreadsheetId=GOOGLE_SHEET_ID,
-                                          range=f'{GOOGLE_SHEET_NAME}!A2:A',
+                                          range=f'{google_sheet_name}!A2:A',
                                           valueInputOption='USER_ENTERED',
                                           body={'values': values}).execute()
 
@@ -284,14 +304,20 @@ def run_backfill() -> None:
                               end_date=end_date)
 
 
-def run_today() -> None:
+def run_now(reporting_period: str) -> None:
     today = datetime.now().strftime(DATE_TIME_FORMAT)
-    MetricCollector().run(start_date='2020-01-01',
-                          end_date=today)
+
+    if reporting_period == 'daily':
+        MetricCollector().run(start_date=today, end_date=today, reporting_period=reporting_period)
+    if reporting_period == 'weekly':
+        start_date = datetime.strptime(today, DATE_TIME_FORMAT) - timedelta(days=7)
+        start_date = start_date.strftime(DATE_TIME_FORMAT)
+        MetricCollector().run(start_date=start_date, end_date=today, reporting_period=reporting_period)
 
 
 if __name__ == '__main__':
-    run_today()
+    reporting_period = sys.argv[1]
+    run_now(reporting_period)
 
     # run_backfill()
 
