85631c1396e87f01bae2db9acc7d157102a6ff0b
Elmer Thomas <ethomas@twilio.com>
2021-07-15 16:32:18 -0700

    move cursor back

--- examples/common/issue.py
+++ examples/common/issue.py
@@ -291,7 +291,8 @@ def get_issues(org: str, repo: str, fragments: str,
 query{{
     search(type: ISSUE,
            first: 50,
-           query: "{issue_type} {issue_state} %cursor% created:{start_date}..{end_date} repo:{org}/{repo}") {{
+           %cursor%,
+           query: "{issue_type} {issue_state} created:{start_date}..{end_date} repo:{org}/{repo}") {{
         nodes {{
             __typename
             {fragments}
