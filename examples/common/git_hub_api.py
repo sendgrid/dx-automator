d243ccaf821acc346ed04dbe72a693abf62de2c6
Sam Harrison <sharrison@twilio.com>
2022-02-16 13:42:39 -0600

    feat: add backoff and retry to GitHub API

--- examples/common/git_hub_api.py
+++ examples/common/git_hub_api.py
@@ -3,7 +3,9 @@ import os
 import re
 from typing import Iterator, Dict
 
+import backoff
 import requests
+from requests import Response
 
 GRAPH_QL_URL = 'https://api.github.com/graphql'
 
@@ -16,8 +18,7 @@ def submit_graphql_query(query: str) -> Iterator[Dict]:
     while True:
         paged_query = substitute(query, {'cursor': f'after: "{cursor}"' if cursor else ''})
 
-        response = requests.post(GRAPH_QL_URL, json={'query': paged_query}, headers=headers)
-        response.raise_for_status()
+        response = post(GRAPH_QL_URL, json={'query': paged_query}, headers=headers)
         response = response.json()
 
         if 'data' not in response:
@@ -32,6 +33,16 @@ def submit_graphql_query(query: str) -> Iterator[Dict]:
 
         if not page_info['hasNextPage']:
             break
+
+
+@backoff.on_exception(backoff.expo,
+                      requests.exceptions.RequestException,
+                      # Keep trying on 403s which indicate rate-limiting.
+                      giveup=lambda e: e.response.status_code != 403)
+def post(url, **kwargs) -> Response:
+    response = requests.post(url, **kwargs)
+    response.raise_for_status()
+    return response
 
 
 def substitute(target: str, values: Dict) -> str:
