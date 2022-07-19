c6f6b223513f0c7da53d59d55c62b39eb1a36989
Sam Harrison <sharrison@twilio.com>
2022-07-19 16:53:28 -0500

    feat: mark pre-release versions as 'Pre-release' in the GitHub release

--- actions/utils/getVersion.ts
+++ actions/utils/getVersion.ts
@@ -1,6 +1,7 @@
 import { Context } from "@actions/github/lib/context";
 
 const REF_REGEX = /^refs\/(.+?)\/(.+)$/;
+const PRE_RELEASE_SEPARATOR = "-";
 
 export default function getVersion(context: Context): string {
   const [ref, refType, refName] = context.ref.match(REF_REGEX) || [];
@@ -13,4 +14,8 @@ export default function getVersion(context: Context): string {
   }
 
   return refName;
+}
+
+export function isPreRelease(version: string): boolean {
+  return version.includes(PRE_RELEASE_SEPARATOR);
 }
