a8b7b8be903d67e815bd67a7ce04431bb2beb9cf
Sam Harrison <sharrison@twilio.com>
2022-09-14 14:17:43 -0500

    [DI-2253] feat: pull main and pre-release branch coverage metrics

--- examples/common/sonar_cloud_api.py
+++ examples/common/sonar_cloud_api.py
@@ -1,9 +1,25 @@
+import re
 from enum import Enum
 from typing import List, Optional
 
 import requests
 
+PROJECT_BRANCHES_URL = 'https://sonarcloud.io/api/project_branches/list'
 COMPONENT_MEASURES_URL = 'https://sonarcloud.io/api/measures/component'
+
+PRE_RELEASE_BRANCH_REGEX = r'\d+\.0\.0-\w+'
+
+
+class ProjectBranch:
+    def __init__(self, branch_data: dict[str, any]) -> None:
+        self.branch_data = branch_data
+
+        self.name = branch_data['name']
+        self.is_main = branch_data['isMain']
+        self.is_pre_release = re.match(PRE_RELEASE_BRANCH_REGEX, self.name) is not None
+
+    def __repr__(self) -> str:
+        return self.name
 
 
 class Metrics(str, Enum):
@@ -13,9 +29,26 @@ class Metrics(str, Enum):
 
 
 class SonarCloudApi:
-    def get_component_measures(self, org: str, repo: str, metric_keys: List[Metrics]) -> Optional[List[dict]]:
+
+    def get_project_branches(self, org: str, repo: str) -> Optional[List[ProjectBranch]]:
+        params = {
+            'project': f'{org}_{repo}',
+        }
+
+        response = requests.get(PROJECT_BRANCHES_URL, params)
+        response = response.json()
+
+        if 'errors' in response:
+            print(response['errors'])
+            return
+
+        return [ProjectBranch(branch) for branch in response['branches']]
+
+    def get_component_measures(self, org: str, repo: str, branch: str,
+                               metric_keys: List[Metrics]) -> Optional[List[dict]]:
         params = {
             'component': f'{org}_{repo}',
+            'branch': branch,
             'metricKeys': ','.join(metric_keys),
         }
 
--- tests/test_sonar_cloud_api.py
+++ tests/test_sonar_cloud_api.py
@@ -1,10 +1,37 @@
 import unittest
 from unittest.mock import patch, Mock
 
-from examples.common.sonar_cloud_api import SonarCloudApi, Metrics, COMPONENT_MEASURES_URL
+from examples.common.sonar_cloud_api import SonarCloudApi, Metrics, COMPONENT_MEASURES_URL, PROJECT_BRANCHES_URL
 
 
 class TestSonarCloudApi(unittest.TestCase):
+    @patch('examples.common.sonar_cloud_api.requests')
+    def test_get_project_branches(self, requests_mock):
+        response_mock = Mock()
+        response_mock.json.return_value = {
+            'branches': [{
+                'name': 'main',
+                'isMain': True,
+            }, {
+                'name': '1.0.0-rc',
+                'isMain': False,
+            }]
+        }
+        requests_mock.get.return_value = response_mock
+
+        response = SonarCloudApi().get_project_branches('erg', 'goo')
+
+        self.assertEqual('main', response[0].name)
+        self.assertEqual(True, response[0].is_main)
+        self.assertEqual(False, response[0].is_pre_release)
+
+        self.assertEqual('1.0.0-rc', response[1].name)
+        self.assertEqual(False, response[1].is_main)
+        self.assertEqual(True, response[1].is_pre_release)
+
+        expected_params = {'project': 'erg_goo'}
+        requests_mock.get.assert_called_once_with(PROJECT_BRANCHES_URL, expected_params)
+
     @patch('examples.common.sonar_cloud_api.requests')
     def test_get_component_measures(self, requests_mock):
         response_mock = Mock()
@@ -15,21 +42,23 @@ class TestSonarCloudApi(unittest.TestCase):
         }
         requests_mock.get.return_value = response_mock
 
-        response = SonarCloudApi().get_component_measures('erg', 'goo',
+        response = SonarCloudApi().get_component_measures('erg', 'goo', 'main',
                                                           [Metrics.LINES_TO_COVER, Metrics.UNCOVERED_LINES])
 
         self.assertEqual('foo', response)
 
-        expected_params = {'component': 'erg_goo', 'metricKeys': 'lines_to_cover,uncovered_lines'}
+        expected_params = {'component': 'erg_goo', 'branch': 'main', 'metricKeys': 'lines_to_cover,uncovered_lines'}
         requests_mock.get.assert_called_once_with(COMPONENT_MEASURES_URL, expected_params)
 
     @patch('examples.common.sonar_cloud_api.requests')
-    def test_get_component_measures_error(self, requests_mock):
+    def test_get_errors(self, requests_mock):
         response_mock = Mock()
         response_mock.json.return_value = {'errors': 'ugh'}
         requests_mock.get.return_value = response_mock
 
-        response = SonarCloudApi().get_component_measures('erg', 'goo',
-                                                          [Metrics.LINES_TO_COVER, Metrics.UNCOVERED_LINES])
+        branches = SonarCloudApi().get_project_branches('erg', 'goo')
+        self.assertIsNone(branches)
 
-        self.assertIsNone(response)
+        measures = SonarCloudApi().get_component_measures('erg', 'goo', 'main',
+                                                          [Metrics.LINES_TO_COVER, Metrics.UNCOVERED_LINES])
+        self.assertIsNone(measures)
